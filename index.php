4fbcbf4e64465104242e22862276f0a20a25fa48
Steven D Milton <55567912+Steven-Milton-CS365@users.noreply.github.com>
2020-12-04 10:46:55 -0500

    Moving index.php to root.

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-echo "Hello world, this is my first heroku app";
-?>
+include __DIR__ .
+'/includes/DatabaseFunctions.php';
+$title = "Elder's Quorum Tools";
+/*ob_start();
+include __DIR__ . '/../templates/elders.html.php';
+$output = ob_get_clean();*/
+include  __DIR__ . '/templates/layout.html.php';?>
--- templates/layout.html.php
+++ templates/layout.html.php
@@ -8,7 +8,7 @@ include_once __DIR__ .
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/eqtools.css">
+  <link rel="stylesheet" href="/css/eqtools.css">
   <title><?=$title?></title>
 </head>
 
