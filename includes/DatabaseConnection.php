930cccf3ffeb3e9e31e8210b6323e77bdae7343a
Steven D Milton <55567912+Steven-Milton-CS365@users.noreply.github.com>
2020-12-04 10:33:29 -0500

    Update DatabaseConnection.php
    lastest updates

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,5 +1,6 @@
 <?php
-    $pdo = new PDO('mysql:host=localhost;dbname=eq_tools;
-    charset=utf8', 'equser', 'Alma3738');
+    //$pdo = new PDO('mysql:host=localhost;dbname=eq_tools;
+   // charset=utf8', 'equser', 'Alma3738');
+    $pdo = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_d5299f62c064191;charset=utf8','bc49b7ac7f9ea4','bbac9840');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
