f710192039ef6afed9b88c6bde6eb37fa90d57d4
Steven D Milton <55567912+Steven-Milton-CS365@users.noreply.github.com>
2020-12-04 22:29:50 -0500

    change menus to flex box

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -6,10 +6,10 @@ include_once __DIR__ .
 <html>
 
 <head>
+  <title><?=$title?></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/css/eqtools.css">
-  <title><?=$title?></title>
 </head>
 
 <body>
